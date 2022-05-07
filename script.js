8adc1210729f928c6b176e0712c82e1c9488ad20
Vanessa Wagner <vmwagner@gmail.com>
2022-05-07 15:18:51 -0500

    testing commit

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Learning to Code";
 
 document.querySelector("body").appendChild(h2);
